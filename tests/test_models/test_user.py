2bc80d99b419210435eb2389328744eb6e2ad52c
Ahmed-D007A <ahmed.mamdouh8840@gmail.com>
2023-07-15 23:36:25 +0300

    adding tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from models.user import User
+
+
+class Test_UserModel(unittest.TestCase):
+    """
+    Test the user model class
+    """
+
+    def setUp(self):
+        self.model = User()
+        self.model.save()
+
+    def test_var_initialization(self):
+        self.assertTrue(hasattr(self.model, "email"))
+        self.assertTrue(hasattr(self.model, "password"))
+        self.assertTrue(hasattr(self.model, "first_name"))
+        self.assertTrue(hasattr(self.model, "last_name"))
+        self.assertEqual(self.model.email, "")
+        self.assertEqual(self.model.password, "")
+        self.assertEqual(self.model.first_name, "")
+        self.assertEqual(self.model.last_name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
